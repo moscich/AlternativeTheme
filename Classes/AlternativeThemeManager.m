8bac0d851a5ee6493deb73f1b5a7f615151224a1
Marek Mościchowski <moscich@gmail.com>
2014-02-22 23:12:46 +0100

    no message

--- Classes/AlternativeThemeManager.m
+++ Classes/AlternativeThemeManager.m
@@ -12,7 +12,14 @@
 }
 - (void)colorLabel:(UILabel *)label withType:(LabelThemeType)type
 {
-    label.textColor = [UIColor whiteColor];
+    if(type == LabelThemeMentalLabel)
+    {
+        label.textColor = [UIColor whiteColor];
+    }
+    if(type == LabelThemeNormalLabel)
+    {
+        label.textColor = [UIColor colorWithRed:194.0/255.0 green:173.0/255.0 blue:118.0/255.0 alpha:1];
+    }
 }
 
 - (void)colorBackground:(UIView *)view
